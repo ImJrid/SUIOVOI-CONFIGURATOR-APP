019e20fbf1dfcb9eefcbef3fa3971642ee31648e
ImJrid <byjj23@gmail.com>
2026-09-03 00:54:31 -0700

    Add files via upload

--- Suiovoi.ps1
+++ Suiovoi.ps1
@@ -3,7 +3,7 @@
 # ============================================================================
 # INSTALL PATHS
 # ============================================================================
-$script:CurrentVersion = "1.6"
+$script:CurrentVersion = "1.7"
 $script:InstallDir     = "C:\SUIOVOI CONFIG"
 $script:InstallPath    = "$script:InstallDir\Suiovoi.ps1"
 $script:ScriptUrl      = "https://raw.githubusercontent.com/ImJrid/SUIOVOI-CONFIGURATOR-APP/main/Suiovoi.ps1"
